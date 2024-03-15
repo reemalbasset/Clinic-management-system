1d6d473eb1973d70d6e3086b2f79d151b1bd6b5b
heba79 <115847405+heba79@users.noreply.github.com>
2024-03-15 15:30:15 +0200

    Typesql

--- demo/src/main/java/com/example/demo/models/User.java
+++ demo/src/main/java/com/example/demo/models/User.java
@@ -4,45 +4,136 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    private String type;
     private String name;
     private String username;
     private String password;
     private String dob;
-    public int getId() {
-        return id;
+
+    public User() {
     }
+
+    public User(int id, String type, String name, String username, String password, String dob) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.dob = dob;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
-    public String getName() {
-        return name;
+
+    public String getType() {
+        return this.type;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getUsername() {
-        return username;
+        return this.username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
-        return password;
+        return this.password;
     }
-    public void setPassword(String passwword) {
-        this.password = passwword;
+
+    public void setPassword(String password) {
+        this.password = password;
     }
+
     public String getDob() {
-        return dob;
+        return this.dob;
     }
+
     public void setDob(String dob) {
         this.dob = dob;
     }
+
+    public User id(int id) {
+        setId(id);
+        return this;
+    }
+
+    public User type(String type) {
+        setType(type);
+        return this;
+    }
+
+    public User name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public User username(String username) {
+        setUsername(username);
+        return this;
+    }
+
+    public User password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    public User dob(String dob) {
+        setDob(dob);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id && Objects.equals(type, user.type) && Objects.equals(name, user.name) && Objects.equals(username, user.username) && Objects.equals(password, user.password) && Objects.equals(dob, user.dob);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, type, name, username, password, dob);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", type='" + getType() + "'" +
+            ", name='" + getName() + "'" +
+            ", username='" + getUsername() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", dob='" + getDob() + "'" +
+            "}";
+    }
+    
 }
 
