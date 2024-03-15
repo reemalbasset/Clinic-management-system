845b68dd4bf3944346a7e30618374a5b7db6fd3d
Reem omar <125567674+reemalbasset@users.noreply.github.com>
2024-03-15 12:41:42 +0200

    registration and login

--- demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,81 @@
 package com.example.demo.controllers;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.demo.models.User;
+import com.example.demo.repositories.UserRepositry;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@RequestMapping("/User")
 public class UserController {
-    
+   @Autowired
+   private UserRepositry userRepositry;
+   
+
+   @GetMapping("Registration")
+   public ModelAndView addUser() {
+    ModelAndView mav=new ModelAndView("registration.html");
+    User newUser=new User();
+    mav.addObject("user", newUser);
+       return mav;
+   }
+   @PostMapping("Registration")
+   public String saveUser(@ModelAttribute User user) {
+      String encoddedPassword=org.mindrot.jbcrypt.BCrypt.hashpw(user.getPassword(),org.mindrot.jbcrypt.BCrypt.gensalt(12));
+      user.setPassword(encoddedPassword);
+      this.userRepositry.save(user);
+       
+       return "added";
+   }
+   @GetMapping("Login")
+   public ModelAndView login() {
+      ModelAndView mav=new ModelAndView("login.html");
+      User newUser=new User();
+      mav.addObject("user", newUser);
+      return mav;
+   }
+   
+   @PostMapping("Login")
+   public RedirectView loginProcess(@RequestParam("username") String username,
+   @RequestParam("password") String password,HttpSession session) {
+
+      User dbUser=this.userRepositry.findByUsername(username);
+       Boolean isPasswordMatched=org.mindrot.jbcrypt.BCrypt.checkpw(password,dbUser.getPassword());
+       if(isPasswordMatched){
+        session.setAttribute("username", dbUser.getUsername());
+       return new RedirectView("Profile");
+       }else
+       return new RedirectView("Login");
+   }
+
+   @GetMapping("Profile")
+   public ModelAndView viewprofile(HttpSession session) {
+     ModelAndView mav = new ModelAndView("Profile.html");
+     mav.addObject("username",(String)session.getAttribute("username"));
+     return mav;
+   }
+   
+   
+   
+   
+   
+   
 }
